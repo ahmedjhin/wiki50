36cd5fd412e1c4dc4bd6bb0fadffeedb503073e9
ahmedjhin <a7madti90@hotmail.com>
2023-03-07 20:44:54 +0400

    rrr
    rrrrr

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
-
+markdowner = Markdown()
+markdowner.convert("*CSS!*")
 def index(request):
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
+    coffee = util.get_entry("coffee")
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(), 'csspage':css_file
     })
 
+
+def csst(request):
+    css_file = util.get_entry("CSS")
+    return render(request,'encyclopedia/css.html')
